844a73f9a7cc9d20bf647f5ec11c50f02c58a824
nicolluisyumang <yumangnicol@gmail.com>
2023-08-03 13:40:08 +0100

    feat: add list view to app

--- hackernews-reader/ContentView.swift
+++ hackernews-reader/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationBarTitle("Hacker News")
         }
-        .padding()
     }
 }
 
@@ -24,3 +23,9 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjur"),
+    Post(id: "3", title: "Kamusta")
+]
---
+++ hackernews-reader/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  hackernews-reader
+//
+//  Created by Nicol Luis Yumang on 8/3/23.
+//
+
+import Foundation
+
+struct Post: Identifiable {
+    let id: String
+    let title: String    
+}
