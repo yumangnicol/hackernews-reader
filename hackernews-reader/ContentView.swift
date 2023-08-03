cfb31184648627f64f5f415bdeb820ee6fe1fd92
nicolluisyumang <yumangnicol@gmail.com>
2023-08-03 13:25:43 +0100

    Initial Commit

---
+++ hackernews-reader/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  hackernews-reader
+//
+//  Created by Nicol Luis Yumang on 8/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hackernews-reader/hackernews_readerApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackernews_readerApp.swift
+//  hackernews-reader
+//
+//  Created by Nicol Luis Yumang on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct hackernews_readerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
